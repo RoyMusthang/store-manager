16d95b1dfe3b0b247d2322d679ec0676d81af492
Josué Lobo <darksiders20005@gmail.com>
2022-01-27 17:16:16 -0300

    perf: configurando o index para trabalhar com o body parser

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const app = express();
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(bodyParser.json());
+const { bodyContains } = require('frisby/src/frisby/expects');
+
+const product = require('./controllers/products')
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
+app.post('/products', product.createProduct)
+
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
-});
+})
