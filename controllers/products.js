0b9da0794dd2b76980e101ac47b3d8184682a919
Josué Lobo <darksiders20005@gmail.com>
2022-01-27 20:05:11 -0300

    refactor: alterando para modo de rotas e validações

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,20 @@
-const productService = require('../services/products');
+const Products = require('express').Router();
+const rescue = require('express-rescue');
 
-const createProduct = async (req, res) => {
+const productService = require('../services/products');
+const {
+  validateName,
+  validateQuantity,
+} = require('./validations');
+
+Products.post('/',
+  validateName,
+  validateQuantity,
+  rescue(async (req, res) => {
     const { name, quantity } = req.body;
     const newProduct = await productService.createProduct(name, quantity);
     res.status(201).json(newProduct);
-  }
+  })
+);
 
-module.exports = {
-  createProduct,
-}
+module.exports = Products;
