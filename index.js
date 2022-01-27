bd09de6dc410e7407c0f548acfbd0e34198907f3
Josué Lobo <darksiders20005@gmail.com>
2022-01-27 20:00:26 -0300

    refactor: mudando o index pra trabalhar com rotas do express

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-const { bodyContains } = require('frisby/src/frisby/expects');
 
-const product = require('./controllers/products')
+const product = require('./controllers/products');
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.post('/products', product.createProduct)
+app.use('/products', product)
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
