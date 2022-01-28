b3bb136eb6c72c8ecd25abdbd7dae11cd79f202f
Josué Lobo <darksiders20005@gmail.com>
2022-01-28 05:04:43 -0300

    fix: arrumando bug da desistruturação do body

--- controllers/products.js
+++ controllers/products.js
@@ -36,9 +36,9 @@ Products.put('/:id',
   validateQuantity,
   rescue(async (req, res) => {
     const { id } = req.params;
+    const { name, quantity } = req.body;
     const attProduct = await productService.attProduct(id, name, quantity);
-    res.status(200).json(attProduct)
-  })
-)
+    res.status(200).json(attProduct);
+  }));
 
 module.exports = Products;
