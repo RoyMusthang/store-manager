ce3da42f6d5f2cac9556bcc2b8f95ff42b1740d1
Josué Lobo <darksiders20005@gmail.com>
2022-01-29 18:12:39 -0300

    feat: adiciona regra de negocio da criação de uma venda na camada de service

--- services/sales.js
+++ services/sales.js
@@ -1,8 +1,21 @@
 const salesModel = require('../models/sales');
 
-async function salesRecord(id, quantity) {
-  const newSales = await salesModel.salesRecord(id, quantity);
-  return newSales;
+async function salesRecord(sales) {
+  const saleId = await salesModel.salesRecord();
+  await Promise.all(sales.map(async (sale) => {
+
+  // o trecho abaixo e uma adaptação criada em cima do código
+  // de Murilo Rainho, a base original pode ser encontrada nessa
+  // thread: https://trybecourse.slack.com/archives/C02NJF4QNH1/p1643412142771639
+  const lintSnakeCase = 'product_id';
+  const {
+    [lintSnakeCase]: productId,
+    quantity,
+  } = sale;
+   await salesModel.create(saleId, productId, quantity);
+
+  }));
+  return { id: saleId, itemsSold: sales };
 }
 
 module.exports = {
