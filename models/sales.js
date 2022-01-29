ddaad4100068e4afb4a6f02bfe7d7ca664325e31
Josué Lobo <darksiders20005@gmail.com>
2022-01-29 18:11:45 -0300

    feat: adiciona funcionalidade de date e createsale na camada de model

---
+++ models/sales.js
@@ -0,0 +1,19 @@
+const connection = require('./connection');
+
+async function salesRecord() {
+  // registra os log(data) da venda
+  const query = 'INSERT INTO sales (date) VALUES (NOW());';
+  const [result] = await connection.execute(query);
+  return result.insertId;
+}
+
+async function create(saleId, productId, quantity) {
+  const query = 'INSERT INTO sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?);';
+  
+  await connection.execute(query, [saleId, productId, quantity]);
+}
+
+module.exports = {
+  create,
+  salesRecord,
+}
