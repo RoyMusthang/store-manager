9c5df1bc683a5b67dc0e9b762f0892557e9c5043
Josué Lobo <darksiders20005@gmail.com>
2022-01-30 13:09:25 -0300

    feat: funções getAll e getById implementadas

--- models/sales.js
+++ models/sales.js
@@ -13,7 +13,30 @@ async function create(saleId, productId, quantity) {
   await connection.execute(query, [saleId, productId, quantity]);
 }
 
+async function getAll() {
+  const query = `
+    SELECT sale_id AS saleId, date, product_id, quantity
+    FROM sales_products  
+    INNER JOIN sales 
+    ON sales.id = sale_id;
+    `;
+  const [result] = await connection.execute(query);
+  return result;
+}
+
+async function getById(saleId) {
+  const query = `
+  SELECT date, product_id, quantity 
+  FROM sales JOIN sales_products
+  ON (sales.id = sale_id)
+  WHERE sale_id = ?`
+  const [sale] = await connection.execute(query, [saleId])
+  return sale;
+}
+
 module.exports = {
   create,
   salesRecord,
+  getAll,
+  getById,
 };
