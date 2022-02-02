ed4eb0bcb9a724746b80478d8ae42d19510bc7b3
Josué Lobo <darksiders20005@gmail.com>
2022-02-02 14:45:25 -0300

    fix: ajustando testes para serem independentes do banco

--- test/unit/controllers.js
+++ test/unit/controllers.js
@@ -1,8 +1,240 @@
 const sinon = require('sinon');
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 const request = require('supertest');
-
+const productsModel = require('../../models/products');
+const connection = require('../../models/connection');
+const salesModel = require('../../models/sales');
 const ProductController = require('../../controllers/products');
+const validatesProduct = require('../../controllers/validations');
+const validatesSale = require('../../controllers/validateSale');
+
+describe('Testando a rotas de products', () => {
+  const res = {};
+  const req = {};
+  const next = () => {};
+
+  describe('Quando não é passado o name pelo body', () => {
+    before(() => {
+      req.body = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 400', async () => {
+      await validatesProduct.validateName(req, res, next);
+
+      expect(res.status.calledWith(400)).to.be.true;
+    });
+  });
+
+  describe('Quando o tamanho de name é menor que 5 caracteres', () => {
+    before(() => {
+      req.body = {name: 'Fone'};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 422', async () => {
+      await validatesProduct.validateName(req, res, next);
+
+      expect(res.status.calledWith(422)).to.be.true;
+    });
+  });
+});
+
+describe('Testando a função validateName', () => {
+  const res = {};
+  const req = {};
+  const next = () => {};
+
+  describe('Quando não é passado o name pelo body', () => {
+    before(() => {
+      req.body = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 400', async () => {
+      await validatesProduct.validateName(req, res, next);
+
+      expect(res.status.calledWith(400)).to.be.true;
+    });
+  });
+
+  describe('Quando o tamanho de name é menor que 5 caracteres', () => {
+    before(() => {
+      req.body = {name: 'Fone'};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 422', async () => {
+      await validatesProduct.validateName(req, res, next);
+
+      expect(res.status.calledWith(422)).to.be.true;
+    });
+  });
+});
+
+describe('Testando a função validateQuantity', () => {
+  const res = {};
+  const req = {};
+  const next = () => {};
+
+  describe('Quando não é passado o quantity pelo body', () => {
+    before(() => {
+      req.body = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 400', async () => {
+      await validatesProduct.validateQuantity(req, res, next);
+
+      expect(res.status.calledWith(400)).to.be.true;
+    });
+  });
+
+  describe('Quando é passada uma string para o quantity', () => {
+    before(() => {
+      req.body = { quantity: 'string' };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 422', async () => {
+      await validatesProduct.validateQuantity(req, res, next);
+
+      expect(res.status.calledWith(422)).to.be.true;
+    });
+  });
+
+  describe('Quando é passado 0 para o quantity', () => {
+    before(() => {
+      req.body = { quantity: 0 };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 422', async () => {
+      await validatesProduct.validateQuantity(req, res, next);
+
+      expect(res.status.calledWith(422)).to.be.true;
+    });
+  });
+});
+
+
+describe('Testando a função validateId', () => {
+  const req = {};
+  const res = {};
+  const next = () => {};
+
+  describe('Quando não é passado o product_id', () => {
+    before(() => {
+      req.body = [{ quantity: 1 }];
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 400', async () => {
+      await validatesSale.validateId(req, res, next);
+
+      expect(res.status.calledWith(400)).to.be.true;
+    });
+  });
+
+  describe('Quando não é passado o quantity', () => {
+    before(() => {
+      req.body = [{ product_id: 1 }];
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 400', async () => {
+      await validatesSale.validateQuantity(req, res, next);
+
+      expect(res.status.calledWith(400)).to.be.true;
+    });
+  });
+
+  describe('Quando quantity é igual a 0', () => {
+    before(() => {
+      req.body = [{ product_id: 1, quantity: 0 }];
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 422', async () => {
+      await validatesSale.validateQuantity(req, res, next);
+
+      expect(res.status.calledWith(422)).to.be.true;
+    });
+  });
+});
+
+describe('Testando a função validade quantity', () => {
+  const req = {};
+  const res = {};
+  const next = () => {};
+
+  describe('Quando quantity é -1', () => {
+    before(() => {
+      req.body = [{ quantity: -1 }];
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 422', async () => {
+      await validatesSale.validateQuantity(req, res, next);
+
+      expect(res.status.calledWith(422)).to.be.true;
+    });
+  });
+
+  describe('Quando quantity é uma string', () => {
+    before(() => {
+      req.body = [{ quantity: 'string' }];
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+
+    it('é chamado com o status 422', async () => {
+      await validatesSale.validateQuantity(req, res, next);
+
+      expect(res.status.calledWith(422)).to.be.true;
+    });
+  });
+});
+
+describe('Testando se não adiciona produto duplicado', () => {
+  const req = [{ name: "jamba" }];
+  const res = {};
+  const next = () => {};
+  describe('Quando passa um produto duplicado', () => {
+    before(() => {
+      req.body = [{ name: "jamba" }];
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+    it('falha', async () => {
+      await validatesProduct.duplicate(req, res, next);
+      expect(res.status.calledWith(409)).to.be.false;
+    })
+  });
+});
 
 describe('Busca de produtos', () => {
   it('Buscando na rota principal', () => {
@@ -36,3 +268,22 @@ describe('Busca de produtos', () => {
       });
   });
 }); 
+
+
+//  Sales Test
+describe('Criando teste para o salesModels', () => {
+  describe('Testando o método "create"', () => {
+    const mockResult = { insertId: 2 }
+    before(() => {
+      sinon.stub(connection, 'query').resolves(mockResult)
+    })
+
+    after(() => {
+      connection.query.restore()
+    })
+
+    it('Testa se o método existe.', () => {
+      assert.exists(salesModel.create());
+    })
+  });
+});
--- test/unit/services.js
+++ test/unit/services.js
@@ -1,87 +1,99 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 const connection = require('../../models/connection');
-const productService = require('../../services/products');
+
+const productsModel = require('../../models/products');
+const salesModel = require('../../models/sales');
+
+const productsService = require('../../services/products');
 const salesService = require('../../services/sales');
 
-describe('Testes na camada de service', () => {
-  beforeEach(async () => {
-    const query = [[]];
 
-    sinon.stub(connection, 'query').resolves(query);
-  });
+// Testes do productsService.
+describe('Testando o camada de service', () => {
+  describe('Testando arquivo de products', () => {
+    mockgetAll = [
+      {
+        "id": 1,
+        "name": "Jamba",
+        "quantity": 10
+      },
+      {
+        "id": 2,
+        "name": "Rivotril",
+        "quantity": 20
+      },
+    ]
 
-  afterEach(async () => connection.query.restore());
+    describe('Testando "productsService.create()"', () => {
 
-  describe('na funcionalidade de getAll', () => {
-    it('retorna um array', async () => {
-      const response = await productService.getAll();
-
-      expect(response).to.be.an('array');
-    });
-  });
-
-  describe('Testa funcionalidade do getAll', () => {
-    it('espera que venha em formato de array', async () => {
-      const response = await salesService.getAll();
-
-      expect(response).to.be.an('array');
-    });
-  });
-    describe('Acrescenta um produto no bando de dados', () => {
-    const payloadProduct = "Gume do Infinito";
-    const pal = 30;
-
-    before(async () => {
-      const execute = [{ insertId: 1 }];
-
-      sinon.stub(connection, 'execute').resolves(execute);
-    });
-
-    after(async () => {
-      connection.execute.restore();
-    });
-
-      it('verifica se retorna o id criado', async () => {
-        const response = await productService.create(payloadProduct, pal);
-
-        expect(response.id).to.be.equal(1);
-      });
-  });
-  describe('Busca um objeto do banco de dados pelo id', () => {
-    describe('Quando não existir nenhum produto cadastrado com o id', () => {
-      before(() => {  
-        sinon.stub(connection, 'execute').resolves([[]]);
-      });
-
-      after(() => {
-        connection.execute.restore();
-      });
-
-      it('retorna null', async () => {
-        const result = await productService.getById(1);
-
-        expect(result).to.be.an('null');
-      });
-
-    });
-    describe('Quando existir um produto cadastrado com o id', () => {
       before(() => {
-        const execute = [{ id: 1, name: "Capuz da Morte de Rabadon", quantity: 30 }];
-
-        sinon.stub(connection, 'execute').resolves(execute);
+        sinon.stub(connection, 'query').resolves([{ id: 1, name: "birulei", quantity: 3 }])
+        sinon.stub(productsModel, 'getAll').resolves([{ name: 'Jamba' }])
+        sinon.stub(productsModel, 'create').resolves({ id: 1, name: "Jhonson", quantity: 3 })
       });
-
       after(() => {
-        connection.execute.restore();
+        productsModel.getAll.restore()
+        productsModel.create.restore()
+        connection.query.restore();
       });
 
-      it('retorna um objeto', async () => {
-        const result = await productService.getById(1);
-
-        expect(result).to.be.an('null');
+      it('Testa se criar um novo produto. ', async () => {
+        const result = await productsService.create({ name: "teste 1", quantity: 3 });
+        expect(result).to.be.all.keys('name', 'id', 'quantity')
       });
+    });
 
+    describe('Testando funcionalidade getAll', () => {
+      before(() => {
+        sinon.stub(productsModel, 'getAll').resolves(mockgetAll)
       });
+      after(() => {
+        productsModel.getAll.restore()
+      });
+      it('Testa se a função retorna uma lista de objetos.', async () => {
+        const resultQuery = await productsService.getAll()
+        expect(mockgetAll).to.be.equal(resultQuery)
+      });
+    });
+
+    describe('Testando "productsService.getById()"', () => {
+      before(() => {
+        sinon.stub(productsModel, 'getById').resolves([mockgetAll[1]])
+      });
+      after(() => {
+        productsModel.getById.restore()
+      });
+      it('Testa se a função retorna o objeto buscando', async () => {
+        const [resultQuery] = await productsService.getById(2)
+        expect(mockgetAll[1]).to.be.equal(resultQuery)
+      });
+    });
   });
-}); 
+
+// Teste do salesService
+  describe('Testando arquivo de sales', () => {
+    describe('Testando funcionalidade getAll', () => {
+      mockgetAll = [
+        {
+          "saleId": 1,
+          "date": "2022-02-01T11:39:40.000Z",
+          "product_id": 1,
+          "quantity": 2
+        },
+        {
+          "saleId": 2,
+          "date": "2022-02-01T11:39:40.000Z",
+          "product_id": 2,
+          "quantity": 4
+        }
+      ]
+      it('Testa se o retorno e um lista de produto.', async () => {
+        sinon.stub(salesModel, 'getAll').resolves(mockgetAll)
+        const expectResult = await salesService.getAll()
+        expect(mockgetAll).to.deep.equal(expectResult);
+      });
+
+    });
+  });
+});
